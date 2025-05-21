63d042e549baba054c40dde8a3a1ba30f40b562c
maurice diong <mdiong00@gmail.com>
2025-05-21 00:42:05 -0400

    starting lab

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function scuberGreetingForFeet(distance){
+// function scuberGreetingForFeet(distance){
   // Write your code here!
   /*describe('scuberGreetingForFeet()', function () {
     it('gives customers a free sample if the ride is less than or equal to 400 feet', function () {
@@ -17,34 +17,34 @@ function scuberGreetingForFeet(distance){
       expect(scuberGreetingForFeet(2501)).to.equal('No can do.');
     });
   });*/
-  if (distance <= 400) {
-    return 'This one is on me!'
-  }
-  else if (distance > 400 && distance <=2000) {
-    return 'That will be twenty bucks.'
-  }
-  else if (distance >2000 && distance <= 2500) {
-    return 'I will gladly take your thirty bucks.'
-  }
-  else {
-    return `No can do.`
-  }
-}
+//   if (distance <= 400) {
+//     return 'This one is on me!'
+//   }
+//   else if (distance > 400 && distance <=2000) {
+//     return 'That will be twenty bucks.'
+//   }
+//   else if (distance >2000 && distance <= 2500) {
+//     return 'I will gladly take your thirty bucks.'
+//   }
+//   else {
+//     return `No can do.`
+//   }
+// }
 
-function ternaryCheckCity(city){
-  // Write your code here!
-  const isNyc = city == 'NYC'? 'Ok, sounds good.': 'No go.';
-  return isNyc;
-}
+// function ternaryCheckCity(city){
+//   // Write your code here!
+//   const isNyc = city == 'NYC'? 'Ok, sounds good.': 'No go.';
+//   return isNyc;
+// }
 
-function switchOnCharmFromTip(tip){
-  // Write your code here!
-  switch (tip) {
-    case `generous`:
-      return `Thank you so much.`
-    case `not as generous`:
-      return `Thank you.`
-    default:
-      return `Bye.`;
-  }
-}
+// function switchOnCharmFromTip(tip){
+//   // Write your code here!
+//   switch (tip) {
+//     case `generous`:
+//       return `Thank you so much.`
+//     case `not as generous`:
+//       return `Thank you.`
+//     default:
+//       return `Bye.`;
+//   }
+// }

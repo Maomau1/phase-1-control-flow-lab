712ded799e6e779fa08acbe7e06228f604b9788a
maurice diong <mdiong00@gmail.com>
2025-05-21 01:01:57 -0400

    3 passings

--- index.js
+++ index.js
@@ -52,7 +52,11 @@ function scuberGreetingForFeet(distance){
   if (distance<=400){
     return 'This one is on me!'
   }
-  else if (distance>400 & distance <=2000){
+  else if (distance>400 && distance <=2000){
     return 'That will be twenty bucks.'
   }
+  else if (distance >2000 && distance <=2500) {
+    return 'I will gladly take your thirty bucks.'
+  }
+
 }
